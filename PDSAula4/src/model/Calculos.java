44fbd3fdf892e87c5f540d49e5559df36612f589
Kauã <kaua.bm@aluno.ifsc.edu.br>
2023-11-08 10:07:35 -0300

    PDSAula4 ainda falta fazer quase toda a parte dos calculos ainda

---
+++ PDSAula4/src/model/Calculos.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Calculos {
+
+	public Float oleo500 (float valor, float quantidade) {
+		float resultado;
+		resultado = valor * quantidade;
+		return resultado;
+	}
+	
+	public Float oleo1L (float valor, float quantidade) {
+		float resultado;
+		resultado = valor * quantidade;
+		return resultado;
+	}
+
+}
--- PDSAula4/src/view/PostoGasolina.java
+++ PDSAula4/src/view/PostoGasolina.java
@@ -8,6 +8,8 @@ import javax.swing.border.EmptyBorder;
 import java.awt.GridLayout;
 import javax.swing.JSplitPane;
 import javax.swing.border.TitledBorder;
+
+import model.Calculos;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.border.EtchedBorder;
 import java.awt.Color;
@@ -19,6 +21,10 @@ import javax.swing.JRadioButton;
 import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class PostoGasolina extends JFrame {
 
@@ -27,8 +33,8 @@ public class PostoGasolina extends JFrame {
 	private JTextField textEtanol;
 	private JTextField textGasAditivada;
 	private JTextField textGasComum;
-	private JTextField textQuantidadeFrasco500;
-	private JTextField textQuantidadeFrasco1;
+	private JTextField textValorFrasco500;
+	private JTextField textValorFrasco1;
 	private JTextField textQuantidade500;
 	private JTextField textQuantidade1;
 	private JTextField textQuantidadeDeLitros;
@@ -58,157 +64,170 @@ public class PostoGasolina extends JFrame {
 		setBounds(100, 100, 609, 556);
 		getContentPane().setLayout(new MigLayout("", "[grow][grow,center]", "[grow][grow][grow][grow]"));
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Tabela de Preco do Combustivel", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		getContentPane().add(panel, "cell 0 0,grow");
-		panel.setLayout(new GridLayout(4, 2, 0, 0));
+		JPanel panelTabelaPrecoCombustivel = new JPanel();
+		panelTabelaPrecoCombustivel.setBorder(new TitledBorder(null, "Tabela de Preco do Combustivel", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		getContentPane().add(panelTabelaPrecoCombustivel, "cell 0 0,grow");
+		panelTabelaPrecoCombustivel.setLayout(new GridLayout(4, 2, 0, 0));
 		
 		JLabel lblOleoDisel = new JLabel("Oleo Disel:");
 		lblOleoDisel.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(lblOleoDisel);
+		panelTabelaPrecoCombustivel.add(lblOleoDisel);
 		
 		textOleoDisel = new JTextField();
-		panel.add(textOleoDisel);
+		panelTabelaPrecoCombustivel.add(textOleoDisel);
 		textOleoDisel.setColumns(10);
 		
 		JLabel lblGasComum = new JLabel("Gas. Comum:");
 		lblGasComum.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(lblGasComum);
+		panelTabelaPrecoCombustivel.add(lblGasComum);
 		
 		textGasComum = new JTextField();
-		panel.add(textGasComum);
+		panelTabelaPrecoCombustivel.add(textGasComum);
 		textGasComum.setColumns(10);
 		
 		JLabel lblGasAditivada = new JLabel("Gas. Aditivada:");
 		lblGasAditivada.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(lblGasAditivada);
+		panelTabelaPrecoCombustivel.add(lblGasAditivada);
 		
 		textGasAditivada = new JTextField();
-		panel.add(textGasAditivada);
+		panelTabelaPrecoCombustivel.add(textGasAditivada);
 		textGasAditivada.setColumns(10);
 		
 		JLabel lblEtanol = new JLabel("Etanol:");
 		lblEtanol.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(lblEtanol);
+		panelTabelaPrecoCombustivel.add(lblEtanol);
 		
 		textEtanol = new JTextField();
-		panel.add(textEtanol);
+		panelTabelaPrecoCombustivel.add(textEtanol);
 		textEtanol.setColumns(10);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Tabela de Preco do Oleo Motor", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		getContentPane().add(panel_1, "cell 1 0,grow");
-		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
+		JPanel panelTabelaPrecoOleoMotor = new JPanel();
+		panelTabelaPrecoOleoMotor.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Tabela de Preco do Oleo Motor", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		getContentPane().add(panelTabelaPrecoOleoMotor, "cell 1 0,grow");
+		panelTabelaPrecoOleoMotor.setLayout(new GridLayout(0, 2, 0, 0));
 		
 		JLabel lblNewLabel_1 = new JLabel("Frasco 500ml");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_1.add(lblNewLabel_1);
+		panelTabelaPrecoOleoMotor.add(lblNewLabel_1);
 		
-		textQuantidadeFrasco500 = new JTextField();
-		panel_1.add(textQuantidadeFrasco500);
-		textQuantidadeFrasco500.setColumns(10);
+		textValorFrasco500 = new JTextField();
+		panelTabelaPrecoOleoMotor.add(textValorFrasco500);
+		textValorFrasco500.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("Frasco 1L");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_1.add(lblNewLabel);
+		panelTabelaPrecoOleoMotor.add(lblNewLabel);
 		
-		textQuantidadeFrasco1 = new JTextField();
-		panel_1.add(textQuantidadeFrasco1);
-		textQuantidadeFrasco1.setColumns(10);
+		textValorFrasco1 = new JTextField();
+		panelTabelaPrecoOleoMotor.add(textValorFrasco1);
+		textValorFrasco1.setColumns(10);
 		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBorder(new TitledBorder(null, "Oleo Motor", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		getContentPane().add(panel_2, "cell 0 1 2 1,grow");
-		panel_2.setLayout(new GridLayout(3, 4, 0, 0));
+		JPanel panelOleoMotor = new JPanel();
+		panelOleoMotor.setBorder(new TitledBorder(null, "Oleo Motor", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		getContentPane().add(panelOleoMotor, "cell 0 1 2 1,grow");
+		panelOleoMotor.setLayout(new GridLayout(3, 4, 0, 0));
 		
 		JPanel panel_5 = new JPanel();
-		panel_2.add(panel_5);
+		panelOleoMotor.add(panel_5);
 		
 		JLabel lblNewLabel_2 = new JLabel("Quantidade");
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_2.add(lblNewLabel_2);
+		panelOleoMotor.add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_6 = new JLabel("Valor a Pagar");
 		lblNewLabel_6.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_2.add(lblNewLabel_6);
+		panelOleoMotor.add(lblNewLabel_6);
 		
 		JLabel lblNewLabel_3 = new JLabel("Total Oleo");
 		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_2.add(lblNewLabel_3);
+		panelOleoMotor.add(lblNewLabel_3);
 		
 		JLabel lblNewLabel_5 = new JLabel("Frascos de 500ml");
 		lblNewLabel_5.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_2.add(lblNewLabel_5);
+		panelOleoMotor.add(lblNewLabel_5);
 		
 		textQuantidade500 = new JTextField();
-		panel_2.add(textQuantidade500);
+		panelOleoMotor.add(textQuantidade500);
 		textQuantidade500.setColumns(10);
 		
 		JLabel lblResultado_1 = new JLabel("-");
+		lblResultado_1.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent e) {
+				
+				
+			}
+		});
 		lblResultado_1.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_2.add(lblResultado_1);
+		panelOleoMotor.add(lblResultado_1);
 		
 		JLabel lblResultado_2 = new JLabel("-");
 		lblResultado_2.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_2.add(lblResultado_2);
+		panelOleoMotor.add(lblResultado_2);
 		
 		JLabel lblNewLabel_4 = new JLabel("Frascos de 1L");
 		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_2.add(lblNewLabel_4);
+		panelOleoMotor.add(lblNewLabel_4);
 		
 		textQuantidade1 = new JTextField();
-		panel_2.add(textQuantidade1);
+		panelOleoMotor.add(textQuantidade1);
 		textQuantidade1.setColumns(10);
 		
 		JLabel lblResultado_4 = new JLabel("-");
+		lblResultado_4.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent e) {
+				Calculos c = new Calculos();
+				float total500 = c.oleo500(textValorFrasco500, textQuantidade500);
+			}
+		});
 		lblResultado_4.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_2.add(lblResultado_4);
+		panelOleoMotor.add(lblResultado_4);
 		
-		JLabel lblResultado_3 = new JLabel("-");
-		lblResultado_3.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_2.add(lblResultado_3);
+		JPanel panel_5_1 = new JPanel();
+		panelOleoMotor.add(panel_5_1);
 		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBorder(new TitledBorder(null, "Abastecimento", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		getContentPane().add(panel_3, "cell 0 2,grow");
-		panel_3.setLayout(new GridLayout(3, 2, 0, 0));
+		JPanel panelTabelaAbastecimento = new JPanel();
+		panelTabelaAbastecimento.setBorder(new TitledBorder(null, "Abastecimento", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		getContentPane().add(panelTabelaAbastecimento, "cell 0 2,grow");
+		panelTabelaAbastecimento.setLayout(new GridLayout(3, 2, 0, 0));
 		
 		JLabel lblNewLabel_8 = new JLabel("Combustivel:");
-		panel_3.add(lblNewLabel_8);
+		panelTabelaAbastecimento.add(lblNewLabel_8);
 		
 		JComboBox comboBox = new JComboBox();
-		panel_3.add(comboBox);
+		panelTabelaAbastecimento.add(comboBox);
 		
 		JLabel lblNewLabel_9 = new JLabel("Quantidade Litros:");
-		panel_3.add(lblNewLabel_9);
+		panelTabelaAbastecimento.add(lblNewLabel_9);
 		
 		textQuantidadeDeLitros = new JTextField();
-		panel_3.add(textQuantidadeDeLitros);
+		panelTabelaAbastecimento.add(textQuantidadeDeLitros);
 		textQuantidadeDeLitros.setColumns(10);
 		
 		JLabel lblNewLabel_7 = new JLabel("Total Combustivel:");
-		panel_3.add(lblNewLabel_7);
+		panelTabelaAbastecimento.add(lblNewLabel_7);
 		
 		JLabel lblResultado_4_1 = new JLabel("-");
 		lblResultado_4_1.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_3.add(lblResultado_4_1);
+		panelTabelaAbastecimento.add(lblResultado_4_1);
 		
-		JPanel panel_4 = new JPanel();
-		panel_4.setBorder(new TitledBorder(null, "Formas de Pagamento", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		getContentPane().add(panel_4, "cell 1 2,grow");
-		panel_4.setLayout(new GridLayout(3, 3, 0, 0));
+		JPanel panelTabelaFormaDePagamento = new JPanel();
+		panelTabelaFormaDePagamento.setBorder(new TitledBorder(null, "Formas de Pagamento", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		getContentPane().add(panelTabelaFormaDePagamento, "cell 1 2,grow");
+		panelTabelaFormaDePagamento.setLayout(new GridLayout(3, 3, 0, 0));
 		
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("Á vista");
-		panel_4.add(rdbtnNewRadioButton);
+		panelTabelaFormaDePagamento.add(rdbtnNewRadioButton);
 		
 		JPanel panel_7 = new JPanel();
-		panel_4.add(panel_7);
+		panelTabelaFormaDePagamento.add(panel_7);
 		
 		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Á prazo");
-		panel_4.add(rdbtnNewRadioButton_1);
+		panelTabelaFormaDePagamento.add(rdbtnNewRadioButton_1);
 		
 		JPanel panel_6 = new JPanel();
-		panel_4.add(panel_6);
+		panelTabelaFormaDePagamento.add(panel_6);
 		
 		JLabel lblNewLabel_10 = new JLabel("Dias:");
 		lblNewLabel_10.setHorizontalAlignment(SwingConstants.CENTER);
@@ -220,30 +239,65 @@ public class PostoGasolina extends JFrame {
 		
 		JLabel lblNewLabel_11 = new JLabel("Total a Pagar:");
 		lblNewLabel_11.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_4.add(lblNewLabel_11);
+		panelTabelaFormaDePagamento.add(lblNewLabel_11);
 		
 		JLabel lblResultado_4_2 = new JLabel("-");
 		lblResultado_4_2.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_4.add(lblResultado_4_2);
+		panelTabelaFormaDePagamento.add(lblResultado_4_2);
 		
-		JPanel panel_8 = new JPanel();
-		getContentPane().add(panel_8, "cell 0 3 2 1,grow");
-		panel_8.setLayout(new GridLayout(0, 5, 0, 0));
+		JPanel panelBotoesDeAcao = new JPanel();
+		getContentPane().add(panelBotoesDeAcao, "cell 0 3 2 1,grow");
+		panelBotoesDeAcao.setLayout(new GridLayout(0, 5, 0, 0));
 		
 		JButton btnButtonCalcular = new JButton("Calcular");
-		panel_8.add(btnButtonCalcular);
+		btnButtonCalcular.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				float vOleoDisel=Float.valueOf(textOleoDisel.getText());
+				float vGasComum=Float.valueOf(textGasComum.getText());
+				float vGasAditivada=Float.valueOf(textGasAditivada.getText());
+				float vEtanol=Float.valueOf(textEtanol.getText());
+				float vValorFrasco500=Float.valueOf(textValorFrasco500.getText());
+				float vValorFrasco1=Float.valueOf(textValorFrasco1.getText());
+				
+				float vQuantidade500=Float.valueOf(textQuantidade500.getText());
+				float vQuantidade1=Float.valueOf(textQuantidade1.getText());
+				
+				float vQuantidadeDeLitros=Float.valueOf(textQuantidadeDeLitros.getText());
+			}
+		});
+		panelBotoesDeAcao.add(btnButtonCalcular);
 		
 		JPanel panel_10 = new JPanel();
-		panel_8.add(panel_10);
+		panelBotoesDeAcao.add(panel_10);
 		
 		JButton btnButtonNovoCalculo = new JButton("Novo Calculo");
-		panel_8.add(btnButtonNovoCalculo);
+		btnButtonNovoCalculo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textOleoDisel.setText("");
+				textGasComum.setText("");
+				textGasAditivada.setText("");
+				textEtanol.setText("");
+				textValorFrasco500.setText("");
+				textValorFrasco1.setText("");
+				textQuantidade500.setText("");
+				textQuantidade1.setText("");
+				textQuantidadeDeLitros.setText("");
+				textQuantidadeDeDias.setText("");
+			}
+		});
+		panelBotoesDeAcao.add(btnButtonNovoCalculo);
 		
 		JPanel panel_9 = new JPanel();
-		panel_8.add(panel_9);
+		panelBotoesDeAcao.add(panel_9);
 		
 		JButton btnButtonFechar = new JButton("Fechar");
-		panel_8.add(btnButtonFechar);
+		btnButtonFechar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+			}
+		});
+		panelBotoesDeAcao.add(btnButtonFechar);
 	}
 
 }
